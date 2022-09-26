023fa7f8aaec1615c1cd82bac023407d077a7de5
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-09-26 11:27:04 -0400

    Update ModalBuilder.ts

--- utils/ModalBuilder.ts
+++ utils/ModalBuilder.ts
@@ -1,51 +1,25 @@
 type Nullable<T> = T | null;
 interface Modal {
-    Title?: string;
-    custom_id?: string;
-    color?: string;
-    author?: object;
-    footer?: object;
-    url?: string;
-    fields?: Array<object>;
-    image?: {url: string};
-    thumbnail?: {url: string};
+    Title: string;
+    custom_id: string;
+    components: Array<any>;
 }
 export type { Modal };
 export default class ModalBuilder {
     data;
     constructor(modal?: Nullable<Modal>){
-        this.data = modal ?? {} as Modal;
+        this.data = modal ?? {title: "", custom_id: "", components: []} as Modal;
     }
     setTitle(title: string){
         this.data.Title = title;
         return this;
     }
-    setDescription(description: string){
-        this.data.description = description;
+    setCustomId(id: string){
+        this.data.custom_id = id;
         return this;
     }
-    setColor(color: string){
-        this.data.color = color;
-        return this;
-    }                   
-    setFooter(footer: object){
-        this.data.footer = footer;
-        return this;
-    }
-    setAuthor(author: object){
-        this.data.author = author;
-        return this;
-    }
-    setUrl(url: string){
-        this.data.url = url;
-        return this;
-    }
-    setImage(image: string){
-        this.data.image = {url: image};
-        return this;
-    }
-    setThumbnail(image: string){
-        this.data.thumbnail= {url: image};
+    addComponents(fields){
+        this.data.components.push(...fields);
         return this;
     }
     toJSON(){
