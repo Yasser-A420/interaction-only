ad855a6e3fb2a2e5020c21e7c326be42348a55cc
Yasser <73202985+Yasser-Alshinawah@users.noreply.github.com>
2022-10-10 10:48:12 -0400

    Update Collector.ts

--- utils/Collector.ts
+++ utils/Collector.ts
@@ -1,6 +1,7 @@
+import { EventEmitter } from "node:events";
 import Interaction from "./Interaction";
 import * as Types from "./typings";
-export default class Collector {
+export default class Collector extends EventEmitter {
     interaction: Interaction
     constructor(interaction: Interaction){
         this.interaction = interaction;
